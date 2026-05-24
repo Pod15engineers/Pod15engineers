f243c9203ed2b35cc01fe9fafcc5cb8a8208143c
Pod15engineers <pod15engineers@gmail.com>
2026-05-24 21:57:56 +0100

    bucket name changed to pod15engineers

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-state-backend-22becd28"
+    bucket = "pod15engineers-tf-state-backend-22becd28"
     key    = "prodution/terraform.tfstate"
     region = "us-east-1"
   }
