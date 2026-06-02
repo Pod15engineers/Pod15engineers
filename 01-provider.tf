7013b25d8a5b2da9194f219912f9c547f7409996
Pod15engineers <pod15engineers@gmail.com>
2026-06-03 00:08:40 +0100

    feat: working infrastructure deployment - VPC, EKS, RDS, DNS, ECR

--- 01-provider.tf
+++ 01-provider.tf
@@ -6,18 +6,18 @@
 terraform {
   required_providers {
     godaddy-dns = {
-      source = "veksh/godaddy-dns"
+      source  = "veksh/godaddy-dns"
       version = "0.3.12"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta2"
     }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.10" # Or any version < 2.11
     }
-  
+
   }
 }
 
--- module-dns/variables.tf
+++ module-dns/variables.tf
@@ -1,17 +1,17 @@
 variable "environment" {
   description = "Deployment environment"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 variable "domain_name" {
   description = "Domain name"
-  type = string
-  default = "pod15engineers.online"
+  type        = string
+  default     = "pod15engineers.online"
 }
 variable "nginx_ingress_lb_dns" {
   description = "DNS name of the NGINX Ingress Load Balancer"
-  type        = string  
-  
+  type        = string
+
 }
 variable "nginx_lb_ip" {
   description = "IP address of the NGINX Ingress Load Balancer"
