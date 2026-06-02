1a0069b35541f9e2a277602cd473794b4b45e54d
Pod15engineers <pod15engineers@gmail.com>
2026-06-02 08:57:29 +0100

    Infrastructure deployment

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -10,11 +10,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0"
+      version = "~> 2.0"    
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = "~> 3.1"
     }
   }
 }
